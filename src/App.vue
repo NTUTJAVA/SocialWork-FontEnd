ba7a93bc2232ddd2d6ebe21da01c666c5605e50a
hevinhsu <57029923+hevinhsu@users.noreply.github.com>
2020-08-23 23:08:53 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <navbar />
+    <div id="picBlock"><img class="center" src="https://placem.at/people?w=2000&h=500" /></div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <Home/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "./components/NavBar.vue";
+import Home from "./components/Home.vue";
+
+export default {
+  name: "App",
+  
+  components: {
+    // HelloWorld,
+    Navbar,
+    Home
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#picBlock {
+        background-color: aquamarine;
+        background-size: cover;
+    }
+</style>
